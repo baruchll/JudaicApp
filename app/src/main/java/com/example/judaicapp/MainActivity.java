f92a579c43ba22a24246f7ddeaf87b2fc2fb68ae
navkc11 <63931144+navkc11@users.noreply.github.com>
2021-06-08 19:22:51 +0300

    הוספתי מתודה חדשה

--- app/src/main/java/com/example/judaicapp/MainActivity.java
+++ app/src/main/java/com/example/judaicapp/MainActivity.java
@@ -11,7 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        test();
 
+
+    }
+
+    private void test() {
 
     }
 }
