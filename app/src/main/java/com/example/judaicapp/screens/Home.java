13cde0b8b33e6c395bbb3d5cf78acb1707c5fa78
Baruch <benmlehrfeld@gmail.com>
2021-07-27 18:33:15 +0300

    added ids on the main page

--- app/src/main/java/com/example/judaicapp/screens/Home.java
+++ app/src/main/java/com/example/judaicapp/screens/Home.java
@@ -2,11 +2,14 @@ package com.example.judaicapp.screens;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.judaicapp.R;
 
@@ -18,7 +21,28 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+            return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        Button siddur, limud, others;
+        siddur = getView().findViewById(R.id.siddur_btn);
+        limud = getView().findViewById(R.id.limud_yomi_btn);
+        others = getView().findViewById(R.id.other_functions_btn);
+
+        siddur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                siddur
+            }
+        });
+    }
+
 }
