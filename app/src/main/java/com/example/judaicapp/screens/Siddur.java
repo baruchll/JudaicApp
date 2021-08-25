a1a346a93e70873a2451707664f17441443b5153
Baruch <benmlehrfeld@gmail.com>
2021-08-25 19:59:12 +0300

    added siddur fragment and siddur nusachim activities

---
+++ app/src/main/java/com/example/judaicapp/screens/Siddur.java
@@ -0,0 +1,32 @@
+package com.example.judaicapp.screens;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.judaicapp.R;
+
+
+public class Siddur extends Fragment {
+
+
+
+    public Siddur() {
+
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/judaicapp/screens/Siddurim/AshkenazSiddur.java
@@ -0,0 +1,16 @@
+package com.example.judaicapp.screens.Siddurim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.judaicapp.R;
+
+public class AshkenazSiddur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ashkenaz_siddur);
+    }
+}
