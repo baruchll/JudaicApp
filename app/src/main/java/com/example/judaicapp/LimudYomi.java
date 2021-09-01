e53fa06fec0c1fc11013a0bd037cffd3792cbddb
Baruch <benmlehrfeld@gmail.com>
2021-09-01 12:48:48 +0300

    added the compass

---
+++ app/src/main/java/com/example/judaicapp/LimudYomi.java
@@ -0,0 +1,29 @@
+package com.example.judaicapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LimudYomi extends Fragment {
+
+
+
+    public LimudYomi() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
