69ed93a700d8a5dd68e03417a843ec01111f7afa
Baruch <benmlehrfeld@gmail.com>
2021-10-21 18:54:34 +0300

    removed the options button

--- app/src/main/java/com/example/judaicapp/HomeScreen.java
+++ app/src/main/java/com/example/judaicapp/HomeScreen.java
@@ -49,12 +49,12 @@ public class HomeScreen extends AppCompatActivity {
         this.loadUser();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_screen, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.home_screen, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onSupportNavigateUp() {
